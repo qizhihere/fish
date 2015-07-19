acd2de1d8842c4abc9788218d82368113966cd14
littleqz <qizhihere@gmail.com>
2015-07-19 16:33:06 +0800

    add realpath check function

--- config.fish
+++ config.fish
@@ -1,55 +1,58 @@
 ########################
 # basic functions
 ########################
-
 function run-once -d "run command if it has not been run."
-    pgrep -u $USER -x $argv[1] >/dev/null; or eval $argv
+	pgrep -u $USER -x $argv[1] >/dev/null; or eval $argv
 end
 
 function run-if -d "run function if condition."
-    # check arguments
-    if [ ! (count $argv) -eq 2 ]
+	# check arguments
+	if [ ! (count $argv) -eq 2 ]
 	echo "error arguments: $argv"
 	echo "Usage: runif func condition"
 	return 1
-    end
+	end
 
-    if eval $argv[2]
+	if eval $argv[2]
 	eval $argv[1]
 	return
-    else
+	else
 	return 1
-    end
+	end
 end
 
 function y-or-n-p -d "ask yes or no and return input test result."
-    if [ (count $argv) -gt 0 ]
+	if [ (count $argv) -gt 0 ]
 	echo $argv
-    end
+	end
 
-    set -l __input_str
-    read __input_str
+	set -l __input_str
+	read __input_str
 
-    if echo $__input_str | grep -i "^y" >/dev/null
+	if echo $__input_str | grep -i "^y" >/dev/null
 	return 0
-    else
+	else
 	return 1
-    end
+	end
 end
 
 function command-exist-p -d "check if a command is existed in system."
-    return (type $argv[1] >/dev/null 2>&1)
+	return (type $argv[1] >/dev/null 2>&1)
 end
 
 function export -d "bash export porting."
-    for i in $argv
+	for i in $argv
 	set -l __export_var (echo $i | grep -o '^[^=]*')
 	if [ "$__export_var" = "$i" ]
-	    set -gx $__export_var $$__export_var
+		set -gx $__export_var $$__export_var
 	else
-	    set -gx $__export_var (echo -n $i | sed -e 's/^[^=]*=//')
+		set -gx $__export_var (echo -n $i | sed -e 's/^[^=]*=//')
 	end
-    end
+	end
+end
+
+if not command-exist-p "realpath"
+	alias realpath "readlink -f"
 end
 
 
@@ -59,8 +62,8 @@ end
 
 # update completion
 if [ ! -e ~/.config/fish/generated_completions ]
-    echo "Updating completions..."
-    fish_update_completions
+	echo "Updating completions..."
+	fish_update_completions
 end
 
 # disable startup greeting
@@ -207,186 +210,185 @@ alias xo "xdg-open"
 ########################
 # extended functions
 ########################
-
 # history
 function h -d "search history and selective copy to system clipboard."
-    set -l __keyword ""
-    if [ "$argv" ]
+	set -l __keyword ""
+	if [ "$argv" ]
 	set __keyword $argv
-    end
+	end
 
-    history | grep "$__keyword" | sed '1!G;h;$!d' | percol | sed 's/^ *[0-9][0-9]* *//p' | tr -d '\n' | cbi
+	history | grep "$__keyword" | sed '1!G;h;$!d' | percol | sed 's/^ *[0-9][0-9]* *//p' | tr -d '\n' | cbi
 end
 
 
 # man page and less
 function man-less-colors -d "set syntax for less and man page."
-    # use gnu source-highlight to replace less
-    if test -e "/usr/bin/src-hilite-lesspipe.sh"
+	# use gnu source-highlight to replace less
+	if test -e "/usr/bin/src-hilite-lesspipe.sh"
 	set -gx LESSOPEN "| /usr/bin/src-hilite-lesspipe.sh %s"
 	set -gx LESS ' -R '
-    end
-    # change man page colors(less), more info reference:
-    #   http://misc.flogisoft.com/bash/tip_colors_and_formatting#terminals_compatibility
-    set -gx LESS_TERMCAP_mb (printf '\e[01;31m')                 # begin blinking
-    set -gx LESS_TERMCAP_md (printf '\e[01;38;5;202m')           # begin bold
-    set -gx LESS_TERMCAP_me (printf '\e[0m')                     # end mode
-    set -gx LESS_TERMCAP_se (printf '\e[0m')                     # end standout-mode
-    set -gx LESS_TERMCAP_so (printf '\e[01;48;5;38;38;5;231m')   # begin standout-mode - info box
-    set -gx LESS_TERMCAP_ue (printf '\e[0m')                     # end underline
-    set -gx LESS_TERMCAP_us (printf '\e[04;38;5;41m')            # begin underline
+	end
+	# change man page colors(less), more info reference:
+	#   http://misc.flogisoft.com/bash/tip_colors_and_formatting#terminals_compatibility
+	set -gx LESS_TERMCAP_mb (printf '\e[01;31m')                 # begin blinking
+	set -gx LESS_TERMCAP_md (printf '\e[01;38;5;202m')           # begin bold
+	set -gx LESS_TERMCAP_me (printf '\e[0m')                     # end mode
+	set -gx LESS_TERMCAP_se (printf '\e[0m')                     # end standout-mode
+	set -gx LESS_TERMCAP_so (printf '\e[01;48;5;38;38;5;231m')   # begin standout-mode - info box
+	set -gx LESS_TERMCAP_ue (printf '\e[0m')                     # end underline
+	set -gx LESS_TERMCAP_us (printf '\e[04;38;5;41m')            # begin underline
 end
 
 
 # emacs
 function e -d "use emacsclient to edit file."
-    if not pgrep -fa "emacs.*?daemon" >/dev/null
+	if not pgrep -fa "emacs.*?daemon" >/dev/null
 	emacs --daemon
-    end
-    if test -z "$argv"
+	end
+	if test -z "$argv"
 	eval $EMACS_PROXY emacsclient -t -a vim
-    else
+	else
 	set path_list ""
 	for i in $argv
-	    set cmd $cmd '-e "(find-file \"'(realpath "$i")'\")"'
+		set cmd $cmd '-e "(find-file \"'(realpath "$i")'\")"'
 	end
 	eval $EMACS_PROXY emacsclient -t -a vim $cmd
-    end
+	end
 end
 
 function pe -d "use proxy version emacsclient to edit."
-    eval "set -gx EMACS_PROXY proxychains; e $argv; set -gx EMACS_PROXY"
+	eval "set -gx EMACS_PROXY proxychains; e $argv; set -gx EMACS_PROXY"
 end
 
 
 # system clipboard
 function cbi -d "copy content to system clipboard."
-    # get content according to number of arguments
-    set -l __content ""
-    switch (count $argv)
+	# get content according to number of arguments
+	set -l __content ""
+	switch (count $argv)
 	case 0
-	    read __content
+		read __content
 	case 1
-	    [ -f $argv[1] -a -r $argv[1] ]; and set __content (cat $argv[1])
+		[ -f $argv[1] -a -r $argv[1] ]; and set __content (cat $argv[1])
 	case '*'
-	    set __content "$argv"
-    end
+		set __content "$argv"
+	end
 
-    # copy to system clipboard
-    if command-exist-p xsel
+	# copy to system clipboard
+	if command-exist-p xsel
 	echo -n $__content | xsel -bi
-    else if command-exist-p xclip
+	else if command-exist-p xclip
 	echo -n $__content | xclip -selection clipboard -i
-    end
+	end
 end
 
 function cbo -d "paste content from system clipboard."
-    if command-exist-p xsel
+	if command-exist-p xsel
 	xsel -bo
-    else if command-exist-p xclip
+	else if command-exist-p xclip
 	xclip -selection clipboard -o
-    end
+	end
 end
 
 
 # nginx
 function ngls
-    set ava_dir /etc/nginx/sites-available/
-    set ena_dir /etc/nginx/sites-enabled/;
-    ls $ava_dir $ena_dir $argv; echo ""; diff -u $ava_dir $ena_dir;
+	set ava_dir /etc/nginx/sites-available/
+	set ena_dir /etc/nginx/sites-enabled/;
+	ls $ava_dir $ena_dir $argv; echo ""; diff -u $ava_dir $ena_dir;
 end
 
 function ngln
-    for i in $argv
+	for i in $argv
 	set conf_dir /etc/nginx
 	set command "sudo ln -sf $conf_dir/sites-available/$i $conf_dir/sites-enabled/$i"
 	echo $command
 	eval $command
-    end
+	end
 end
 
 function ngrm
-    for i in $argv
+	for i in $argv
 	set conf_dir /etc/nginx
 	set command "sudo rm -f $conf_dir/sites-enabled/$i"
 	echo $command
 	eval $command
-    end
+	end
 end
 
 
 # fish
 function rel
-    . ~/.config/fish/config.fish
+	. ~/.config/fish/config.fish
 end
 
 
 # easy extract and compress
 function extract -d "automatically extract from archive according to extension."
-    if [ -f "$argv" ]
+	if [ -f "$argv" ]
 	switch $argv
-	    case '*.tar.bz2' '*.tbz2'
+		case '*.tar.bz2' '*.tbz2'
 		tar xvjf $argv
-	    case '*.tar.gz' '*.tgz'
+		case '*.tar.gz' '*.tgz'
 		tar xvzf $argv
-	    case '*.bz2'
+		case '*.bz2'
 		bunzip2 $argv
-	    case '*.rar'
+		case '*.rar'
 		rar x $argv
-	    case '*.gz'
+		case '*.gz'
 		gunzip $argv
-	    case '*.tar'
+		case '*.tar'
 		tar xvf $argv
-	    case '*.zip' '*.apk' '*.epub' '*.xpi' '*.war' '*.jar'
+		case '*.zip' '*.apk' '*.epub' '*.xpi' '*.war' '*.jar'
 		unzip $argv
-	    case '*.Z'
+		case '*.Z'
 		uncompress $argv
-	    case '*.7z'
+		case '*.7z'
 		7z x $argv
-	    case '*'
+		case '*'
 		echo "don't know how to extract '$argv'..."
 	end
-    end
+	end
 end
 
 function compress
-    if [ (count $argv) -ge 2 ]
+	if [ (count $argv) -ge 2 ]
 	switch $argv[1]
-	    case '*.tar'
+		case '*.tar'
 		tar cf $argv[1] $argv[2..-1]
-	    case '*.tar.bz2'
+		case '*.tar.bz2'
 		tar cjf $argv[1] $argv[2..-1]
-	    case '*.tar.gz' '*.tgz'
+		case '*.tar.gz' '*.tgz'
 		tar czf $argv[1] $argv[2..-1]
-	    case '*.zip'
+		case '*.zip'
 		zip $argv[1] $argv[2..-1]
-	    case '*.rar'
+		case '*.rar'
 		rar $argv[1] $argv[2..-1]
 	end
-    end
+	end
 end
 
 
 
 # docker
 function docker-enter
-    sudo nsenter --target (docker-pid $argv[1]) --mount --uts --ipc --net --pid $argv[2..-1]
+	sudo nsenter --target (docker-pid $argv[1]) --mount --uts --ipc --net --pid $argv[2..-1]
 end
 
 function den
-    sudo docker exec -it $argv[1] sh
+	sudo docker exec -it $argv[1] sh
 end
 
 function denf
-    sudo docker exec -it $argv[1] env TERM=xterm-256color fish
+	sudo docker exec -it $argv[1] env TERM=xterm-256color fish
 end
 
 function drum
-    sudo docker run --rm -it -v (realpath ./):/host $argv sh
+	sudo docker run --rm -it -v (realpath ./):/host $argv sh
 end
 
 function drumf
-    sudo docker run --rm -it -v (realpath ./):/host $argv env TERM=xterm-256color fish
+	sudo docker run --rm -it -v (realpath ./):/host $argv env TERM=xterm-256color fish
 end
 
 #xrdb -merge $HOME/.Xresources
