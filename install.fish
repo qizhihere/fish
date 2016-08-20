6a84753c119bd5c10e1d5e9d1d0f4c2cf552bcf4
littleqz <qizhihere@gmail.com>
2016-08-20 11:42:21 +0800

    Modify install script.

--- install.fish
+++ install.fish
@@ -7,11 +7,22 @@ function has -d "check if a command is existed in system."
     return 0
 end
 
+function verbose_do
+    echo "$argv"
+    eval "$argv"
+end
+
 # install oh my fish
 if begin not has omf; and [ -z "$OMF_BOOTSTRAP" ]; end
     curl -L https://github.com/oh-my-fish/oh-my-fish/raw/master/bin/install | env OMF_BOOTSTRAP=yes fish
 end
 
+set CUR (realpath (dirname (status -f)))
+set DST "$HOME/.config/omf"
+if [ "$CUR" != "$DST" ]
+    verbose_do "ln -s \"$CUR\" \"$DST\""
+end
+
 # create shortcuts
-ln -sf ~/.config/omf/init.fish ~/.fishrc
-ln -sf ~/.config/omf/.dircolors ~/
+verbose_do "ln -sf ~/.config/omf/init.fish ~/.fishrc"
+verbose_do "ln -sf ~/.config/omf/.dircolors ~/.dircolors"
